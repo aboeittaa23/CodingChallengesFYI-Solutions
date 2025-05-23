f63d73097e7449ffbfccf86a22ea69ae6267e758
aboeittaa23 <aboeittaa23@mail.wlu.edu>
2025-05-22 22:47:06 -0400

    exit with proper error code

--- challenges/01-wc-tool/main.cpp
+++ challenges/01-wc-tool/main.cpp
@@ -3,7 +3,12 @@
 int main(int argc, char *argv[])
 {
     CCWC ccwc_obj;
-    ccwc_obj.parseArgs(argc, argv);
+
+    if (!ccwc_obj.parseArgs(argc, argv))
+    {
+        return 1;
+    }
+
     ccwc_obj.evaluate();
 
     return 0;
