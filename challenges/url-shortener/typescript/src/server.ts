52479cd50b39b96fa62f91b926c4ddfbaa53af84
aboeittaa23 <aboeittaa23@mail.wlu.edu>
2025-06-02 17:07:16 -0400

    setup prettier

--- challenges/url-shortener/typescript/src/server.ts
+++ challenges/url-shortener/typescript/src/server.ts
@@ -5,9 +5,9 @@ const app: Express = express();
 const PORT = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+    console.log(`Example app listening on port ${PORT}`);
 });
