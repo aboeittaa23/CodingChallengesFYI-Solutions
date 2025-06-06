bbfeba30bbec1d1b9fcd9b0c140352302c9dc298
aboeittaa23 <aboeittaa23@mail.wlu.edu>
2025-06-06 00:54:02 -0400

    restructure tokenizer class

--- challenges/json-parser/src/tokenizer.cpp
+++ challenges/json-parser/src/tokenizer.cpp
@@ -1,76 +1,51 @@
+#include "tokenizer.h"
 #include <iostream>
 
 using namespace std;
 
-enum TokenType
+Tokenizer::Tokenizer(const string &input) : json(input),
+                                            position(0) {}
+
+Token Tokenizer::getNextToken()
 {
-    END_OF_FILE,
-    INT,
-    STRING,
-    OBJECT_START, // {
-    OBJECT_END,   // }
-    COLON,        // :
-    COMMA,        // ,
-    OTHER
-};
-
-struct Token
-{
-    TokenType type;
-    string value;
-};
-
-class Tokenizer
-{
-private:
-    string json;
-    int position;
-
-public:
-    Tokenizer(const string &input) : json(input),
-                                     position(0) {}
-
-    Token getNextToken()
+    if (position >= json.size())
     {
-        if (position >= json.size())
-        {
-            return Token{END_OF_FILE, ""};
-        }
+        return Token{END_OF_FILE, ""};
+    }
 
-        Token token;
-        char c = json[position];
+    Token token;
+    char c = json[position];
 
-        if (c == '{')
+    if (c == '{')
+    {
+        token = {OBJECT_START, string(1, c)};
+    }
+    else if (c == '}')
+    {
+        token = {OBJECT_END, string(1, c)};
+    }
+    else if (c == ',')
+    {
+        token = {COMMA, string(1, c)};
+    }
+    else if (c == ':')
+    {
+        token = {COLON, string(1, c)};
+    }
+    else if (isdigit(c))
+    {
+        token.type = INT;
+        while (position < json.length() && isdigit(json[position]))
         {
-            token = {OBJECT_START, string(1, c)};
+            token.value += json[position];
+            position++;
         }
-        else if (c == '}')
-        {
-            token = {OBJECT_END, string(1, c)};
-        }
-        else if (c == ',')
-        {
-            token = {COMMA, string(1, c)};
-        }
-        else if (c == ':')
-        {
-            token = {COLON, string(1, c)};
-        }
-        else if (isdigit(c))
-        {
-            token.type = INT;
-            while (position < json.length() && isdigit(json[position]))
-            {
-                token.value += json[position];
-                position++;
-            }
-            return token;
-        }
-        else
-        {
-            token = {OTHER, string(1, c)};
-        }
-        position++;
         return token;
     }
-};
+    else
+    {
+        token = {OTHER, string(1, c)};
+    }
+    position++;
+    return token;
+}
---
+++ challenges/json-parser/src/tokenizer.h
@@ -0,0 +1,38 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include <iostream>
+
+using namespace std;
+
+enum TokenType
+{
+    END_OF_FILE,
+    INT,
+    STRING,
+    OBJECT_START, // {
+    OBJECT_END,   // }
+    COLON,        // :
+    COMMA,        // ,
+    OTHER
+};
+
+struct Token
+{
+    TokenType type;
+    string value;
+};
+
+class Tokenizer
+{
+private:
+    string json;
+    int position;
+
+public:
+    Tokenizer(const string &input);
+
+    Token getNextToken();
+};
+
+#endif
